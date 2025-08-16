71826040785fd75ea56f9067d83bbe19162277a0
Jesse Holzman <jesseholzman96@gmail.com>
2025-08-16 15:49:06 -0400

    Add docstring and remaining method declarations for Field

--- src/Field.hpp
+++ src/Field.hpp
@@ -3,6 +3,11 @@
 
 #include "raylib-cpp.hpp"
 
+/*! An abstraction for treating a 2D grid of values as a continuous field.
+ *
+ * Can be sampled at any point and the result will be a linear combination of
+ * the four nearest cells in the underlying grid.
+ */
 template<typename T>
 class Field
 {
@@ -15,7 +20,9 @@ class Field
     T GetCell(int x, int y);
     void SetCell(int x, int y, T value);
 
-    T GetAtPoint(raylib::Vector2 position);
+    T Get(raylib::Vector2 position);
+    raylib::Vector2 Gradient(raylib::Vector2 position);
+    void Modify(raylib::Vector2 position, T delta);
 
   private:
     T* data;
