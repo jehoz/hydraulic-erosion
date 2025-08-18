a9c9c6760a4eec71b82932d58eb404e355c693e2
holzmanj <jesseholzman96@gmail.com>
2025-08-18 17:44:53 -0400

    Start implementing simulation class

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,8 +1,36 @@
 #include "Simulation.hpp"
 
-WaterParticle::WaterParticle(raylib::Vector2 pos)
+WaterParticle::WaterParticle(raylib::Vector2 position)
+  : position(position)
 {
-    position = pos;
     velocity = raylib::Vector2(0.0f, 0.0f);
     volume = 1.0f;
+}
+
+Simulation::Simulation(int meshResolution)
+  : terrain_height(ScalarField(meshResolution, meshResolution))
+{
+    // setup default heightmap generator
+    FastNoiseLite noise;
+    noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
+    noise.SetFractalType(FastNoiseLite::FractalType_Ridged);
+    noise.SetFractalGain(0.5f);
+    noise.SetFractalOctaves(6);
+    noise.SetFrequency(0.0075 / 2.0);
+
+    terrain_height = ScalarField(meshResolution, meshResolution);
+
+    raylib::Image heightmap(meshResolution, meshResolution, BLACK);
+    for (int y = 0; y < meshResolution; y++) {
+        for (int x = 0; x < meshResolution; x++) {
+            float height =
+              noise.GetNoise(static_cast<float>(x) / meshResolution,
+                             static_cast<float>(y) / meshResolution);
+            // remap from [-1, 1] to [0, 1]
+            height = (height + 1.0) / 2.0;
+
+            terrain_height.SetCell(x, y, height);
+        }
+    }
+    heightmap_tex = LoadTextureFromImage(heightmap);
 }
--- src/Simulation.hpp
+++ src/Simulation.hpp
@@ -1,7 +1,15 @@
 #ifndef SIMULATION_HPP
 #define SIMULATION_HPP
 
-#include "raylib-cpp.hpp"
+#include "FastNoiseLite.h"
+#include "ScalarField.hpp"
+#include "raylib-cpp.hpp" // IWYU pragma: keep
+
+struct InitialTerrain
+{
+    FastNoiseLite noise;
+    int resolution = 256;
+};
 
 struct WaterParticle
 {
@@ -10,6 +18,18 @@ struct WaterParticle
     float volume;
 
     WaterParticle(raylib::Vector2 position);
+};
+
+class Simulation
+{
+    ScalarField terrain_height;
+
+    raylib::Texture heightmap_tex;
+
+    raylib::Model model;
+
+  public:
+    Simulation(int meshResolution = 256);
 };
 
 #endif
