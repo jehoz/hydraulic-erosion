9aefd5a1c2a86ea29c2889cd6b94decbeb433018
holzmanj <jesseholzman96@gmail.com>
2025-08-25 17:16:27 -0400

    Move UnorderedArena implementation into header (so templating works)

--- src/UnorderedArena.hpp
+++ src/UnorderedArena.hpp
@@ -1,39 +1,66 @@
 #ifndef UNORDERED_ARENA_HPP
 #define UNORDERED_ARENA_HPP
 
+#include "raylib.h"
 #include <cstddef>
-#include <iterator>
 
 /*! A fixed-capacity arena that uses swapback for removals, making it pretty
  * quick to remove arbitrary elements, but it doesn't preserve the order of its
  * contents.
  */
 template<typename T>
-struct UnorderedArena
+class UnorderedArena
 {
-    struct Iterator
-    {
-        using iterator_category = std::forward_iterator_tag;
-        using differece_type = std::ptrdiff_t;
-        using value_type = T;
-        using pointer = T*;
-        using reference = T&;
-    };
 
-    const int& size = size_;
-    const int& capacity = capacity_;
-
-    UnorderedArena<T>(int capacity);
-    ~UnorderedArena();
-
-    void Add(T);
-    void Remove(int index);
-
-  private:
     T* data;
 
-    int size_;
-    int capacity_;
+    size_t size_;
+    size_t capacity_;
+
+  public:
+    const size_t& size = size_;
+    const size_t& capacity = capacity_;
+
+    UnorderedArena(size_t capacity)
+      : capacity_(capacity)
+      , size_(0)
+    {
+        data = new T[capacity];
+    }
+
+    ~UnorderedArena() { delete[] data; }
+
+    T& operator[](size_t i) { return data[i]; }
+
+    using iterator = T*;
+    iterator begin() { return &data[0]; }
+    iterator end() { return &data[size_]; }
+
+    void Add(T elem)
+    {
+        if (size >= capacity) {
+            TraceLog(LOG_ERROR, "UnorderedArena::Add arena is full");
+            return;
+        }
+
+        data[size] = elem;
+        size_++;
+    }
+
+    void Remove(size_t index)
+    {
+        if (index >= size) {
+            TraceLog(LOG_ERROR, "UnorderedArena::Remove index out of bounds");
+            return;
+        }
+
+        // no need to swapback if only a single element
+        if (size > 1) {
+            data[index] = data[size - 1];
+        }
+
+        size_--;
+    }
 };
 
 #endif
